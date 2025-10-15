d29da3c48435f27c8158009b563055d65d854f28
AndrxsQ <andresquintana2004@gmail.com>
2025-10-15 17:04:50 -0500

    Modifying default texts and info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,15 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-6 text-center">
-        <p className="text-lg font-semibold mb-4">Conéctate</p>
+        <p className="text-lg font-semibold mb-4">Connect</p>
         <div className="flex justify-center space-x-6 mb-6">
           {/* Reemplaza con tus enlaces y usa iconos SVG o de bibliotecas como React-Icons */}
-          <a href="[URL_LINKEDIN]" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">LinkedIn</a>
-          <a href="[URL_GITHUB]" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">GitHub</a>
-          <a href="mailto:[TU_CORREO]" className="text-gray-400 hover:text-indigo-400 transition duration-300">Email</a>
+          <a href="https://www.linkedin.com/in/andr%C3%A9s-quintana-b205592b4/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">LinkedIn</a>
+          <a href="https://github.com/AndrxsQ" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">GitHub</a>
+          <a href="mailto:andresquintana2004@gmail.com" className="text-gray-400 hover:text-indigo-400 transition duration-300">Email</a>
         </div>
         <p className="text-sm text-gray-500">
-          © {new Date().getFullYear()} [Tu Nombre]. Desarrollado con React y Tailwind CSS.
+          © {new Date().getFullYear()} Andrés Quintana Alape. Developed with React and Tailwind CSS.
         </p>
       </div>
     </footer>
