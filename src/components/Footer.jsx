c4077b7ce3a010dd2d4dbb793fb0037cbbfec8bb
AndrxsQ <andresquintana2004@gmail.com>
2025-10-15 17:05:23 -0500

    Deleting an unnecessary comment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ const Footer = () => {
       <div className="container mx-auto px-6 text-center">
         <p className="text-lg font-semibold mb-4">Connect</p>
         <div className="flex justify-center space-x-6 mb-6">
-          {/* Reemplaza con tus enlaces y usa iconos SVG o de bibliotecas como React-Icons */}
           <a href="https://www.linkedin.com/in/andr%C3%A9s-quintana-b205592b4/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">LinkedIn</a>
           <a href="https://github.com/AndrxsQ" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">GitHub</a>
           <a href="mailto:andresquintana2004@gmail.com" className="text-gray-400 hover:text-indigo-400 transition duration-300">Email</a>
