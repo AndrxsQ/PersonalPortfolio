3953dddda5f4340b0b1568f2a81834946491b7fe
AndrxsQ <andresquintana2004@gmail.com>
2025-10-15 17:20:38 -0500

    Changing Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
       <div className="container mx-auto px-6 text-center">
         <p className="text-lg font-semibold mb-4">Connect</p>
         <div className="flex justify-center space-x-6 mb-6">
-          <a href="https://www.linkedin.com/in/andr%C3%A9s-quintana-b205592b4/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">LinkedIn</a>
-          <a href="https://github.com/AndrxsQ" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-indigo-400 transition duration-300">GitHub</a>
-          <a href="mailto:andresquintana2004@gmail.com" className="text-gray-400 hover:text-indigo-400 transition duration-300">Email</a>
+          <a href="https://www.linkedin.com/in/andr%C3%A9s-quintana-b205592b4/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-yellow-400 transition duration-300">LinkedIn</a>
+          <a href="https://github.com/AndrxsQ" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-yellow-400 transition duration-300">GitHub</a>
+          <a href="mailto:andresquintana2004@gmail.com" className="text-gray-400 hover:text-yellow-400 transition duration-300">Email</a>
         </div>
         <p className="text-sm text-gray-500">
           © {new Date().getFullYear()} Andrés Quintana Alape. Developed with React and Tailwind CSS.
